ebd9bd4831601d7cf73e3acbc6c4af7f8fd879df
CauaRodriguesS <caua20005@gmail.com>
2023-09-17 23:15:37 -0300

    exercicio 1 pronto

--- exercicios/exercicio 12/script/codigo.js
+++ exercicios/exercicio 12/script/codigo.js
@@ -6,13 +6,17 @@ var hora = data.getHours()
 var texto = document.querySelector("#texto")
 msg.innerHTML = `agora sao ${hora} horas` 
 if(hora >= 0 && hora < 12){
-    img.src = "../imagens/manhã.jpeg"
+    img.src = "manha.jpeg"
     texto.innerHTML = "Bom dia!"
 }
-else if(hora >= 12 && hora > 18){
+else if(hora >= 12 && hora < 18){
+    img.src = "tarde.jpeg"
     texto.innerHTML = "Boa tarde!"
+    document.body.style.backgroundColor = '#e9c398'
 }
 else{
+    img.src = "noite].jpeg"
     texto.innerHTML = "Boa noite!"
+    document.body.style.background = '#152c41'
 }
 }
