30849511b39fde46fa32fdb91898bdfedfe25ee4
CauaRodriguesS <caua20005@gmail.com>
2023-10-17 23:33:59 -0300

    aaaaa

--- exercicios/ex17/script/codigo.js
+++ exercicios/ex17/script/codigo.js
@@ -1,18 +1,18 @@
 function tabuada(){
-     let num = document.getElementById('txtn')
-     let tab = document.getElementById('seltab')
-    if(num.Value.length == 0){
-        
-    }else{
-        let n = Number(num.value)
-        let c = 1
-        while(c <= 10){
-            let item = document.createElement('option')
-            item.text = ` ${n} x ${ c} = ${ c * n}`
-            tab.appendChild(item)
-            c++
-        }
-    }
-     
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+   if(num.Value.length == 0){
+       
+   }else{
+       let n = Number(num.value)
+       let c = 1
+       while(c <= 10){
+           let item = document.createElement('option')
+           item.text = ` ${n} x ${ c} = ${ c * n}`
+           tab.appendChild(item)
+           c++
+       }
+   }
+    
 
 }
