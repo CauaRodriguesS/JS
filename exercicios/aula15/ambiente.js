24dbb284e5c98eaf1a0e183552e4a8a62d9cd71a
CauaRodriguesS <caua20005@gmail.com>
2023-10-22 13:30:28 -0300

    resolvi o problema e açFIUBGFPIU

---
+++ exercicios/aula15/ambiente.js
@@ -0,0 +1,10 @@
+let num = [ 7, 4 ,2, 43, 5]
+/* for(let a = 0; a < num.length; a++){
+    console.log(num[a])
+}*/
+ 
+for(let pos in num){
+    console.log(num.indexOf(8))
+}
+
+
--- exercicios/ex17/script/codigo.js
+++ exercicios/ex17/script/codigo.js
@@ -1,11 +1,13 @@
 function tabuada(){
     let num = document.getElementById('txtn')
     let tab = document.getElementById('seltab')
-   if(num.Value.length == 0){
+   if(num.value.length == 0 || num.value.length == NaN){
        alert('erro')
    }else{
+    alert("passou")
        let n = Number(num.value)
        let c = 1
+       tab.innerHTML = " "
        while(c <= 10){
            let item = document.createElement('option')
            item.text = ` ${n} x ${ c} = ${ c * n}`
